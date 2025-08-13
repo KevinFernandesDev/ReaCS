f29c00b737cee8d17a9ba8b23a930d0f46d9b71a
kevinfernandesdev <33194308+KevinFernandesDev@users.noreply.github.com>
2025-08-13 20:06:23 +0200

    Update StyleTranslateBoxed.cs

--- Runtime/Core/StyleTranslateBoxed.cs
+++ Runtime/Core/StyleTranslateBoxed.cs
@@ -1,51 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
-using System;
 
 namespace ReaCS.Runtime.Core
 {
     [Serializable]
     public class StyleTranslateBoxed
     {
-        [Observable] public Observable<float> x = new();
-        [Observable] public Observable<float> y = new();
-        [Observable] public Observable<float> z = new();
-
-        [Observable] public Observable<LengthUnit> unitX = new() { Value = LengthUnit.Pixel };
-        [Observable] public Observable<LengthUnit> unitY = new() { Value = LengthUnit.Pixel };
-
-        [Observable] public Observable<StyleKeyword> keyword = new() { Value = StyleKeyword.Undefined };
+        public Observable<StyleLength> x = new();
+        public Observable<StyleLength> y = new();
+        public Observable<float> z = new();
+        public Observable<StyleKeyword> keyword = new();
 
         public StyleTranslate ToStyleTranslate()
         {
             return new StyleTranslate
             {
-                value = new Translate(
-                    new Length(x.Value, unitX.Value),
-                    new Length(y.Value, unitY.Value),
-                    z.Value
-                ),
+                value = new Translate(x.Value.value, y.Value.value, z.Value),
                 keyword = keyword.Value
             };
         }
 
-        public void FromStyleTranslate(StyleTranslate styleTranslate)
+        public void SetFrom(StyleTranslate translate)
         {
-            x.Value = styleTranslate.value.x.value;
-            y.Value = styleTranslate.value.y.value;
-            z.Value = styleTranslate.value.z;
-
-            unitX.Value = styleTranslate.value.x.unit;
-            unitY.Value = styleTranslate.value.y.unit;
-            keyword.Value = styleTranslate.keyword;
-        }
-
-        public static implicit operator StyleTranslate(StyleTranslateBoxed b) => b?.ToStyleTranslate() ?? default;
-        public static implicit operator StyleTranslateBoxed(StyleTranslate v)
-        {
-            var b = new StyleTranslateBoxed();
-            b.FromStyleTranslate(v);
-            return b;
+            x.Value = new StyleLength(translate.value.x);
+            y.Value = new StyleLength(translate.value.y);
+            z.Value = translate.value.z;
+            keyword.Value = translate.keyword;
         }
     }
 }
